af9350c5421613b8f28da2dc38fc0ef7680d6057
suchanmyung <aud248@naver.com>
2022-04-19 23:40:16 +0900

    codingTest Day-8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,37 @@ package main.java;
 import java.util.Scanner;
 
 /**
- * N개의 수로 이루어진 수열이 주어짐
- * 이 수열에서 연속 부분 수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구해라
+ * 입력으로 양의 정수 N이 입력되면 2개 이상의 연속된 자연수의 합으로
+ * 정수 N을 표현하는 방법의 가짓수를 출력하는 프로그램을 작성하세요.
  *
- * 만약 N=8이고 M=6이고 수열이 1 2 1 3 1 1 1 2면
- * 합이 6이 되는 연속 부분 수열은 {2,1,3}, {1,3,1,1} {3,1,1,1}로 세개임
+ * 만약 N = 15이면
+ * 7+ 8 = 15
+ * 4 + 5 + 6 = 15
+ * 1+2+3+4+5 = 15와 같이 총 3가지의 경우가 조냊한다.
  */
+
 public class Main {
-    public int solution(int n, int m, int[] arr){
-        int answer = 0;
-        int sum =0;
-        int lt = 0;
-        for (int rt = 0; rt < n; rt++) {
-            sum += arr[rt]; //rt를 증가시키면서 sum에누적
-            if(sum==m) answer++; //sum이 m값과 같으면 answer++
-            while (sum >= m) { // sum이 m보다 크거나 같을 동안 계속 반복
-                sum -= arr[lt++]; // sum에서 arr[lt] 값을 빼고 lt를 더함
-                if(sum==m) answer++; // 만약 sum==m이 되면 answer을 더하고, while문 나감
-            }
+
+    public int solution(int n) {
+        int answer=0;
+        int sum=0;
+        int lt =0;
+        int m = n/2+1;
+
+        int[] arr = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            arr[i] = i+1;
         }
 
+        for (int rt = 0; rt < m; rt++) {    //오른쪽 포인터 증가 반복문
+            sum += arr[rt]; // 오른쪽 포인터의 값을 sum에 누적
+            if(sum==n) answer++; // sum == n이면 answer 증가
+            while (sum >= n) { // sum이 n보다 큰 경우 반복
+                sum -= arr[lt++]; // 왼쪽 포인터 값을 빼주고 포인터 이동
+                if(sum==n) answer++;
+            }
+        }
 
         return answer;
     }
@@ -31,14 +42,7 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int m = kb.nextInt();
 
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = kb.nextInt();
-        }
-
-        System.out.println(T.solution(n,m,arr));
+        System.out.println(T.solution(n));
     }
 }
---
+++ src/main/java/Subsequence.java
@@ -0,0 +1,44 @@
+package main.java;
+
+import java.util.Scanner;
+
+/**
+ * N개의 수로 이루어진 수열이 주어짐
+ * 이 수열에서 연속 부분 수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구해라
+ *
+ * 만약 N=8이고 M=6이고 수열이 1 2 1 3 1 1 1 2면
+ * 합이 6이 되는 연속 부분 수열은 {2,1,3}, {1,3,1,1} {3,1,1,1}로 세개임
+ */
+public class Subsequence {
+    public int solution(int n, int m, int[] arr){
+        int answer = 0;
+        int sum =0;
+        int lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt]; //rt를 증가시키면서 sum에누적
+            if(sum==m) answer++; //sum이 m값과 같으면 answer++
+            while (sum >= m) { // sum이 m보다 크거나 같을 동안 계속 반복
+                sum -= arr[lt++]; // sum에서 arr[lt] 값을 빼고 lt를 더함
+                if(sum==m) answer++; // 만약 sum==m이 되면 answer을 더하고, while문 나감
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Subsequence T = new Subsequence();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(T.solution(n,m,arr));
+    }
+}
